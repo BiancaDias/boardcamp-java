707a7cd0e9d8ab3c6db830d6cffb08745270988f
Bianca Dias <bianca.dias@ufv.br>
2024-02-04 14:08:13 -0300

    feat: game service

--- src/main/java/com/boardcamp/api/service/GameService.java
+++ src/main/java/com/boardcamp/api/service/GameService.java
@@ -1,8 +1,28 @@
 package com.boardcamp.api.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.boardcamp.api.dtos.GameDTO;
+import com.boardcamp.api.models.GameModel;
+import com.boardcamp.api.repository.GameRepository;
 
 @Service
 public class GameService {
-  
+  final GameRepository gameRepository;
+
+  GameService(GameRepository gameRepository){
+    this.gameRepository = gameRepository;
+  }
+
+  public List<GameModel> findAll(){
+    return gameRepository.findAll();
+  }
+
+  public Optional<GameModel> save(GameDTO dto){
+    GameModel game = new GameModel(dto);
+    return Optional.of(gameRepository.save(game));
+  }
 }
