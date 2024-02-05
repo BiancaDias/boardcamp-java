b494cfa6661aba0872330fc7e20e30b76554a4f3
Bianca Dias <bianca.dias@ufv.br>
2024-02-04 21:08:21 -0300

    feat: query to find cpf

--- src/main/java/com/boardcamp/api/repository/CustomerRepository.java
+++ src/main/java/com/boardcamp/api/repository/CustomerRepository.java
@@ -1,9 +1,13 @@
 package com.boardcamp.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.boardcamp.api.models.CustomerModel;
 
 public interface CustomerRepository extends JpaRepository<CustomerModel, Long> {
-  
+
+  @Query("SELECT CASE WHEN COUNT(g) > 0 THEN true ELSE false END FROM CustomerModel c WHERE c.cpf = :customerCpf")
+  boolean existsByCpf(@Param("customerCpf") String customerCpf);
 }
