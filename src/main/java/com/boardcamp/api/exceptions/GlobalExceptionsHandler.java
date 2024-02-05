4dc15d115484dd4b98f808c9cee92edcc8599283
Bianca Dias <bianca.dias@ufv.br>
2024-02-04 21:17:42 -0300

    feat: error not found

--- src/main/java/com/boardcamp/api/exceptions/GlobalExceptionsHandler.java
+++ src/main/java/com/boardcamp/api/exceptions/GlobalExceptionsHandler.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionsHandler {
   @ExceptionHandler({ NameConflictException.class })
-  public ResponseEntity<String> handlerGameNameConflict(NameConflictException exception) {
+  public ResponseEntity<String> handlerNameConflict(NameConflictException exception) {
     return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
   }
+
+  @ExceptionHandler({ NotFoundException.class })
+public ResponseEntity<String> handlerNotFound(NotFoundException exception) {
+  return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+}
 }
 
